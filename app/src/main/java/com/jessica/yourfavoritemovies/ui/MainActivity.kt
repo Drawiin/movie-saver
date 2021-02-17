73475564b34b1ccf4f9fb4d1994ba86e16726231
vinicius santos <viniciussantos@jera.com.br>
2021-02-17 15:08:09 -0400

    splash ready

--- app/src/main/java/com/jessica/yourfavoritemovies/ui/MainActivity.kt
+++ app/src/main/java/com/jessica/yourfavoritemovies/ui/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_YourFavoriteMovies)
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         window.run {
